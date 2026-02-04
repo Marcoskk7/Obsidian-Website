ab92659b71112aa10d43229b18480d73694c1133
marcoskk7 <marcoskk7@qq.com>
2026-02-05 00:28:39 +0800

    Quartz sync: Feb 5, 2026, 12:28 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "marcoskk7.github.io/Obsidian-Website",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
